10c2fd8f4d3fca1361e90db27dbbc5da4f153fdc
meganemura <meganemura@users.noreply.github.com>
2015-02-23 00:17:28 +0900

    Implement Issuehub::Client#pulls

--- lib/issuehub/client.rb
+++ lib/issuehub/client.rb
@@ -14,5 +14,14 @@ module Issuehub
       @github_client = Octokit.client
     end
     attr_reader :github_client
+
+    # owner/repo
+    def repository
+      ENV['ISSUEHUB_REPOSITORY']
+    end
+
+    def pulls
+      @pulls = github_client.pulls(repository)
+    end
   end
 end
